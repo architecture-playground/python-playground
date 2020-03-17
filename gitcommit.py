4aec31d1069e981e8594cb8b4e84817a1e14c2aa
Bulgakov Anton <bulgakov.anton.a@gmail.com>
2020-03-17 17:41:03 +0200

    Optimize script, inline REST api calls

--- gitcommit.py
+++ gitcommit.py
@@ -1,18 +1,11 @@
 import os
-import requests
 
-file = "sha_save.txt"
-filecommit = "Commit_sha_and_commit_messages.txt"
-myfilecommit = open(filecommit, mode='w', encoding='Latin_1')
-os.system("git log --pretty=format:%H,%s >> Commit_sha_and_commit_messages.txt ")
-myfile = open(file, mode='w', encoding='Latin_1')
-os.system("git log --pretty=format:%H  >> sha_save.txt")
-myfile = open(file, mode='r', encoding='Latin_1')
-dir = os.path.basename(os.getcwd())
-headers = {'Authorization': 'Basic QnVsZ2Frb3ZBbnRvbjpBa2FtZWdha2lsbDkw'}
+commitsInfoFileName = "commit_sha_and_commit_messages.txt"
+os.system("git log --pretty=format:%H,%s >> " + commitsInfoFileName)
 
-for line in myfile:
-    line = requests.get('https://api.github.com/repos/architecture-playground/' + dir.strip() +'/git/commits/' + line.strip(), headers=headers)
-    print(line.text)
-myfile.close()
-os.system("rm -rf sha_save.txt")
+commitInfoFile = open(commitsInfoFileName, mode='r', encoding='UTF-8')
+
+for fileLine in commitInfoFile :
+    print(fileLine.strip())
+
+os.system("rm -rf " + commitsInfoFileName)
