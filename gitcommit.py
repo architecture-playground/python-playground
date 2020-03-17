6a25c4280cb738e31c2d4ea1925c0fecbb7a8293
Bulgakov Anton <bulgakov.anton.a@gmail.com>
2020-03-17 17:21:23 +0200

    add headers

--- gitcommit.py
+++ gitcommit.py
@@ -9,8 +9,10 @@ myfile = open(file, mode='w', encoding='Latin_1')
 os.system("git log --pretty=format:%H  >> sha_save.txt")
 myfile = open(file, mode='r', encoding='Latin_1')
 dir = os.path.basename(os.getcwd())
+headers = {'Authorization': 'Basic QnVsZ2Frb3ZBbnRvbjpBa2FtZWdha2lsbDkw'}
+
 for line in myfile:
-    line = requests.get('https://api.github.com/repos/architecture-playground/' + dir.strip() +'/git/commits/' + line.strip())
+    line = requests.get('https://api.github.com/repos/architecture-playground/' + dir.strip() +'/git/commits/' + line.strip(), headers=headers)
     print(line.text)
 myfile.close()
 os.system("rm -rf sha_save.txt")
